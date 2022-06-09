7fd722acc06ff2d4de952d127f1f372801375d60
Kelvin Chin <chinkaiwen@yahoo.com>
2022-06-10 03:10:31 +0800

    Added emoji data for generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hello world');
+import emojis from 'emojibase-data/en/data.json' assert { type: 'json' };
+
+emojis.forEach(emoji => {
+    console.log({ label: emoji.label, emoji: emoji.emoji });
+});

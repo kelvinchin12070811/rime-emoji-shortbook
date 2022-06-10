6311fccbe79276b04b344007fecfcae1acfe8102
Kelvin Chin <chinkaiwen@yahoo.com>
2022-06-11 02:53:47 +0800

    Added procedure to write meta schema

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
-/**
- * Output location of the generated schema files.
- */
-export const OUT_LOCATION = './dist';
+export const OUTPUT_LOCATION = './dist';
+export const SCHEMA_HEADER = `
+# Rime default settings
+# Rime schema: My First Cool Schema
+# Rime dictionary: Lingua Latina
+`;
+
+export const SCHEMA_AUTHORS = ['Kelvin Chin <kelvinchin12070811.gitlab.io>'];
+export const SCHEMA_DESCRIPTION = '一個簡單的表情輸入方案，以 Discord 式短語輸入為主';
+export const SCHEMA_ID = 'emoji_shortbook';
+export const SCHEMA_NAME = 'Emoji Shortbook';
+export const SCHEMA_VERSION = '1.0.0.0';
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,42 @@
 import emojis from 'emojibase-data/en/data.json' assert { type: 'json' };
 import shortcodes from 'emojibase-data/en/shortcodes/joypixels.json' assert { type: 'json' };
 import fs from 'fs';
+import YAML from 'yaml';
 
-import { OUT_LOCATION } from './config';
+import * as config from './config';
+
+const buildSchema = () => {
+    const schema = {
+        schema_id: config.SCHEMA_ID,
+        name: config.SCHEMA_NAME,
+        version: config.SCHEMA_VERSION,
+        author: config.SCHEMA_AUTHORS,
+        description: config.SCHEMA_DESCRIPTION,
+    };
+
+    const file = YAML.stringify({ schema });
+    const hwndFile = fs.openSync(`${config.OUTPUT_LOCATION}/emoji_shortbook_schema.yaml`, 'w');
+    fs.writeFileSync(hwndFile, `${config.SCHEMA_HEADER.trim()}\n${file}`);
+    fs.closeSync(hwndFile);
+};
 
 const builder = () => {
-    if (fs.existsSync(OUT_LOCATION)) {
+    if (fs.existsSync(config.OUTPUT_LOCATION)) {
         console.log('Removing previous build...');
-        fs.rmSync(OUT_LOCATION, { recursive: true, force: true });
+        fs.rmSync(config.OUTPUT_LOCATION, { recursive: true, force: true });
     }
 
-    console.log('Building...');
-    fs.mkdirSync(OUT_LOCATION);
+    fs.mkdirSync(config.OUTPUT_LOCATION);
 
-    const dict = fs.openSync(`${OUT_LOCATION}/dict.txt`, 'w');
+    console.log('Generating schema metadata...');
+    buildSchema();
+
+    console.log('Building...');
+    const dict = fs.openSync(`${config.OUTPUT_LOCATION}/dict.txt`, 'w');
     emojis.forEach(emoji => {
         fs.writeFileSync(dict, `${shortcodes[emoji.hexcode]}\t${emoji.emoji}\n`);
     });
+    fs.closeSync(dict);
 };
 
 builder();
